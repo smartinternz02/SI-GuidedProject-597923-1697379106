278a00e4c57e7ddd7c2f6c5224fbdbdde80c3b33
devanshu0602 <devanshugupta0602@gmail.com>
2023-10-30 13:25:50 +0530

    Updated app.py - updated the model and calculations

--- 4. Project Development Phase/app.py
+++ 4. Project Development Phase/app.py
@@ -38,27 +38,29 @@ def login():
     total_sales = float(request.form["total_sales"])
 
     # Calculating the required values
-    attr5 = 365 * ((cash + short_term_securities + receivables_short_term_liabilities) / operating_expenses_depreciation)
-    attr6 = retained_earnings / total_assets
-    attr26 = (net_profit + depreciation) / total_liabilities
-    attr27 = profit_on_operating_activities / financial_expenses
-    attr34 = operating_expenses / total_liabilities
-    attr35 = profit_on_sales / total_assets
-    attr42 = profit_on_operating_activities / sales
-    attr46 = current_assets_inventory / short_term_liabilities
-    attr56 = sales_cost_of_products_sold / sales
-    attr58 = total_costs / total_sales
+    attr5 = 365 * float(((cash + short_term_securities + receivables_short_term_liabilities) / operating_expenses_depreciation))
+    attr6 = float(retained_earnings / total_assets)
+    attr26 = float((net_profit + depreciation) / total_liabilities)
+    attr27 = float(profit_on_operating_activities / financial_expenses)
+    attr34 = float(operating_expenses / total_liabilities)
+    attr35 = float(profit_on_sales / total_assets)
+    attr42 = float(profit_on_operating_activities / sales)
+    attr46 = float(current_assets_inventory / short_term_liabilities)
+    attr56 = float(sales_cost_of_products_sold / sales)
+    attr58 = float(total_costs / total_sales)
 
     # Making an input array for the model
     inputs = [[attr5, attr6, attr26, attr27, attr34, attr35, attr42, attr46, attr56, attr58]]
 
     # Getting the prediction
     prediction = model.predict(inputs)
-    print(prediction)
-    if prediction == 1:
+
+    # Find out if bankrupt or not
+    if prediction[0] == 1:
         output = "!! THE COMPANY CAN GO BANKRUPT !!"
     else:
         output = "!! THE COMPANY IS SAFE !!"
+    print(output)
 
     # Returning the prediction
     return jsonify({'y': output})
@@ -66,24 +68,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-
-# Attr5: [(cash + short-term securities + receivables short-term liabilities) / (operating expenses depreciation)] * 365
-
-# Attr6: retained earnings / total assets
-
-# Attr26: (net profit + depreciation) / total liabilities
-
-# Attr27: profit on operating activities / financial expenses
-
-# Attr34: operating expenses / total liabilities
-
-# Attr35: profit on sales / total assets
-
-# Attr42: profit on operating activities / sales
-
-# Attr46: (current assets inventory) / short-term liabilities
-
-# Attr56: (sales cost of products sold) / sales
-
-# Attr58: total costs /total sales
