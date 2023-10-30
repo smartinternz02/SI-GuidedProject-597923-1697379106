82ecaab44d2172eed311f84b9b039bee6429249c
devanshu0602 <devanshugupta0602@gmail.com>
2023-10-30 11:59:55 +0530

    Updated app.py

--- 4. Project Development Phase/app.py
+++ 4. Project Development Phase/app.py
@@ -6,7 +6,7 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('XGB_model.pkl', 'rb'))
+model = pickle.load(open('predictor.pkl', 'rb'))
 
 
 @app.route('/')
@@ -17,19 +17,40 @@ def start():
 @app.route('/login', methods=['POST'])
 def login():
     # Getting the input values from the application
-    attr1 = float(request.form["attr1"])
-    attr2 = float(request.form["attr2"])
-    attr3 = float(request.form["attr3"])
-    attr4 = float(request.form["attr4"])
-    attr5 = float(request.form["attr5"])
-    attr6 = float(request.form["attr6"])
-    attr7 = float(request.form["attr7"])
-    attr8 = float(request.form["attr8"])
-    attr9 = float(request.form["attr9"])
-    attr10 = float(request.form["attr10"])
+    cash = float(request.form["cash"])
+    short_term_securities = float(request.form["short_term_securities"])
+    receivables_short_term_liabilities = float(request.form["receivables_short_term_liabilities"])
+    operating_expenses_depreciation = float(request.form["operating_expenses_depreciation"])
+    retained_earnings = float(request.form["retained_earnings"])
+    total_assets = float(request.form["total_assets"])
+    net_profit = float(request.form["net_profit"])
+    depreciation = float(request.form["depreciation"])
+    total_liabilities = float(request.form["total_liabilities"])
+    profit_on_operating_activities = float(request.form["profit_on_operating_activities"])
+    financial_expenses = float(request.form["financial_expenses"])
+    operating_expenses = float(request.form["operating_expenses"])
+    profit_on_sales = float(request.form["profit_on_sales"])
+    sales = float(request.form["sales"])
+    current_assets_inventory = float(request.form["current_assets_inventory"])
+    short_term_liabilities = float(request.form["short_term_liabilities"])
+    sales_cost_of_products_sold = float(request.form["sales_cost_of_products_sold"])
+    total_costs = float(request.form["total_costs"])
+    total_sales = float(request.form["total_sales"])
+
+    # Calculating the required values
+    attr5 = 365 * ((cash + short_term_securities + receivables_short_term_liabilities) / operating_expenses_depreciation)
+    attr6 = retained_earnings / total_assets
+    attr26 = (net_profit + depreciation) / total_liabilities
+    attr27 = profit_on_operating_activities / financial_expenses
+    attr34 = operating_expenses / total_liabilities
+    attr35 = profit_on_sales / total_assets
+    attr42 = profit_on_operating_activities / sales
+    attr46 = current_assets_inventory / short_term_liabilities
+    attr56 = sales_cost_of_products_sold / sales
+    attr58 = total_costs / total_sales
 
     # Making an input array for the model
-    inputs = [[attr1, attr2, attr3, attr4, attr5, attr6, attr7, attr8, attr9, attr10]]
+    inputs = [[attr5, attr6, attr26, attr27, attr34, attr35, attr42, attr46, attr56, attr58]]
 
     # Getting the prediction
     prediction = model.predict(inputs)
@@ -44,4 +65,25 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+
+
+# Attr5: [(cash + short-term securities + receivables short-term liabilities) / (operating expenses depreciation)] * 365
+
+# Attr6: retained earnings / total assets
+
+# Attr26: (net profit + depreciation) / total liabilities
+
+# Attr27: profit on operating activities / financial expenses
+
+# Attr34: operating expenses / total liabilities
+
+# Attr35: profit on sales / total assets
+
+# Attr42: profit on operating activities / sales
+
+# Attr46: (current assets inventory) / short-term liabilities
+
+# Attr56: (sales cost of products sold) / sales
+
+# Attr58: total costs /total sales
